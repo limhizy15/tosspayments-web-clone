8d66cf8df9269f294ca2bbc8c097942a5313df2d
limhizy15 <limhizy15@ajou.ac.kr>
2023-10-11 23:28:00 +0900

    [#2] refactor: 라우터 구성 class 기반으로 수정

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,9 @@
-import { RouterMap, createRouter } from './router'
+import { Router } from './router'
 
-const routerMap: RouterMap = {
-  home: {
-    path: '/home',
-    html: '<span>HOME</span>',
-  },
-  about: {
-    path: '/about',
-    html: '<span>ABOUT</span>',
-  },
+// TODO: 컴포넌트로 맵핑하기
+const routerMap = {
+  '/home': '<span>HOME</span>',
+  '/about': '<span>ABOUT</span>',
 }
 
-const router = createRouter(routerMap)
-
-window.addEventListener('DOMContentLoaded', (e) => {
-  router.navigate(window.location.pathname.replace('/', ''))
-})
-
-// TODO: header 컴포넌트로 분리
-const $header = document.getElementById('header')
-
-$header?.addEventListener('click', (e) => {
-  const target = e.target as HTMLButtonElement
-  router.navigate(target.id)
-})
+new Router({ routes: routerMap })
--- src/router.ts
+++ src/router.ts
@@ -1,21 +1,43 @@
-// TODO: html이 아닌 컴포넌트를 넣도록 수정
-export type RouterMap = Record<string, { path: string; html: string }>
+interface RouterProps {
+  // TODO: path에 맞는 string이 아닌 컴포넌트를 맵핑하도록
+  routes: Record<string, string>
+}
 
-export const createRouter = (routerMap: RouterMap) => {
-  const _routerMap = routerMap
+export class Router {
+  routes: Record<string, string>
 
-  const $root = document.getElementById('root')
+  constructor({ routes }: RouterProps) {
+    this.routes = routes
+    this.bindEvent()
+  }
 
-  return {
-    navigate: (pathname: string) => {
-      const foundRoute = _routerMap?.[pathname]
+  bindEvent() {
+    document.body.addEventListener('click', (e) => {
+      const targetElement = e.target as HTMLElement
 
-      if (foundRoute) {
-        history.pushState(null, '', foundRoute.path)
-        if (!!$root) {
-          $root.innerHTML = foundRoute.html
-        }
+      if (targetElement.matches('[data-spa-link]')) {
+        e.preventDefault()
+        const newURL = (targetElement as HTMLAnchorElement).href
+
+        history.pushState(null, '', newURL)
+        this.renderPage()
       }
-    },
+    })
+
+    window.addEventListener('load', () => this.renderPage())
+    window.addEventListener('popstate', () => this.renderPage())
+  }
+
+  renderPage() {
+    const path = window.location.pathname
+    const page = this.routes[path]
+
+    const $root = document.getElementById('root') as HTMLElement
+
+    if (page == null) {
+      $root.innerHTML = `<h1>404</h1>`
+    } else {
+      $root.innerHTML = page
+    }
   }
 }
